a3937e5be4093bbba322c45686be2cf5d92cbc3d
Corwin <corstreet@gmail.com>
2017-04-09 15:01:27 -0700

    adding some basic routing for the storefront

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,22 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import {BrowserRouter, Match, Miss} from 'react-router';
 
-render(<App/>, document.getElementById('main'));
+import App from './components/App';
+import Storepicker from './components/Storepicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={Storepicker} />
+				<Match pattern="/store/:storeId" component={App} />
+				<Miss component={NotFound} />	
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.getElementById('main'));
